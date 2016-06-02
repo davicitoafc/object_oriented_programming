86777451c9a0dd72bb6d15481cbbd17ddf2272f7
David Cisneros <d.cisneros849@gmail.com>
2016-06-02 16:34:21 -0400

    Created method to display meal time

--- cat.rb
+++ cat.rb
@@ -5,7 +5,17 @@ class Cat
     @preferred: preferred_foods
     @meal_time: meal_time
   end
+
+  def eats_at
+    if meal_time < 12
+      puts "#{meal_time} AM"
+  elsif meal_time > 12
+    puts "#{meal_time} PM"
+  end
+
 end
 
 cat = Cat.new('fluffy', 'fish', 2)
 cat2 = Cat.new('snookums', 'birds', 4)
+
+eats_at(cat)
