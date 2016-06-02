dc7ca2634397761418d534acb6906b5b56fab2cf
David Cisneros <d.cisneros849@gmail.com>
2016-06-02 16:26:33 -0400

    created initialize method and added attributes

---
+++ cat.rb
@@ -0,0 +1,11 @@
+class Cat
+
+  def initialize(name, preferred_foods, meal_time)
+    @name: name
+    @preferred: preferred_foods
+    @meal_time: meal_time
+  end
+end
+
+cat = Cat.new('fluffy', 'fish', 2)
+cat2 = Cat.new('snookums', 'birds', 4)
