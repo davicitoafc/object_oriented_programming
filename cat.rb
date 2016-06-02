29155c708e3ce719c763b24dfea4b38407816743
David Cisneros <d.cisneros849@gmail.com>
2016-06-02 17:19:09 -0400

    created meow method and interpolated meal_time and name

--- cat.rb
+++ cat.rb
@@ -1,21 +1,41 @@
+#Created Cat Class
 class Cat
 
   def initialize(name, preferred_foods, meal_time)
-    @name: name
-    @preferred: preferred_foods
-    @meal_time: meal_time
+    @name = name
+    @preferred = preferred_foods
+    @meal_time = meal_time
   end
 
+#Created method that states what times each cat eats
   def eats_at
-    if meal_time < 12
-      puts "#{meal_time} AM"
-  elsif meal_time > 12
-    puts "#{meal_time} PM"
+    if @meal_time < 12
+      puts "#{@meal_time} AM"
+    elsif @meal_time > 12
+      puts "#{@meal_time - 12} PM"
+    elsif @meal_time == 12
+      puts "12 PM"
+    elsif @meal_time == 0 || @meal_time == 24
+      puts "12 AM"
+    end
+    puts " "
   end
 
+#Created a method that displays introduction from each cat
+  def meow
+  puts "My name is #{@name} and I eat tuna at #{@meal_time}"
+  puts " "
+  end
 end
 
-cat = Cat.new('fluffy', 'fish', 2)
-cat2 = Cat.new('snookums', 'birds', 4)
+#Created instances of cat class
+cat1 = Cat.new('fluffy', 'fish', 12)
+cat2 = Cat.new('snookums', 'birds', 16)
 
-eats_at(cat)
+#calls eats_at method on cats
+cat1.eats_at
+cat2.eats_at
+
+#Calls meow method on cats
+cat1.meow
+cat2.meow
