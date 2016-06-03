2bd81bb6aef77bf1629e3c7f0b264c8cb31c46cf
David Cisneros <d.cisneros849@gmail.com>
2016-06-02 23:18:30 -0400

    created a variable on line 13 that accepts number from user and adds it to qouta array

--- paperboy.rb
+++ paperboy.rb
@@ -4,12 +4,22 @@ class Paperboy
 
   def initialize
     @name = name
-    @quota = 50
+    @quota = []
     @earnings = []
   end
 
   def qouta
+    puts "Enter qouta for the day: "
+    qouta = 50 + (gets.chomp.to_i)
+    @qouta = qouta 
     puts "#{@name}'s quota is: #{@qouta}"
+  end
+
+  def earnings
+  end
+
+  def report
+    puts "I'm Tommy, I've delivered #{@qouta} "
   end
 end
 
@@ -19,3 +29,5 @@ tommy.name = "Tommy"
 
 #Calling qouta on tommy
 tommy.qouta
+
+#Calling tommy on earnings method
