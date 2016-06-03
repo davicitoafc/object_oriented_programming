db5057c87fc86447aa44b28f54ccefc1f7f6d308
David Cisneros <d.cisneros849@gmail.com>
2016-06-02 22:31:14 -0400

    layed out the structure of quota, initialize, deliver and report methods

---
+++ paperboy.rb
@@ -0,0 +1,26 @@
+class Paperboy
+  attr_reader :name
+  attr_accessor 
+
+  def initialize
+    @Name =
+    @Quota =
+    @Experience =
+    @Side =
+    @Earnings =
+  end
+
+  def qouta
+
+  end
+
+  def deliver(start_address, end_address)
+
+  end
+
+  def report
+
+  end
+
+
+end
