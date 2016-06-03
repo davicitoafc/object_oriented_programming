f97895a77c98e4b63c0c42901af41557e408d9ee
David Cisneros <d.cisneros849@gmail.com>
2016-06-02 23:38:12 -0400

    labeled code and entered earnings per_paper on line 13

--- paperboy.rb
+++ paperboy.rb
@@ -8,16 +8,22 @@ class Paperboy
     @earnings = []
   end
 
+#delivery method that calculates pay based on range of house numbers
   def delivery(start_address, end_address)
-
+    per_paper = 0.25
   end
 
+#qouta method retrieves number, adds it to qouta array, and puts out qouta for the day
   def qouta
+    puts "Enter qouta for the day: "
+    qouta = 50 + (gets.chomp.to_i)
+    @qouta = qouta
     puts "#{@name}'s quota is: #{@qouta}"
   end
 
+#report method puts earnings based on qouta 
   def report
-    puts "I'm #{@name}, I've delivered:#{@qouta} papers"
+    puts "I'm #{@name}, I've delivered: #{@qouta} papers"
     puts "Based on this paper route, I've earned:"
   end
 end
